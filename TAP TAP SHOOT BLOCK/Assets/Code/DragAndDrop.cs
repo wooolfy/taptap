0f56efb8b8b38bfdb0e49dbc306b43480e09caca
Georg Wolf <webmaster@wooolfy.de>
2018-12-23 11:50:22 +0100

    working version without sound

---
+++ TAP TAP SHOOT BLOCK/Assets/Code/DragAndDrop.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Gets the world position of the mouse on the screen        
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var tapCount = Input.touchCount;
+        var touch1 = new Touch();
+        var touch2 = new Touch();
+        if (tapCount > 1)
+        {
+            touch1 = Input.GetTouch(0);
+            touch2 = Input.GetTouch(1);
+            Vector2 newDirection;
+            if (this.transform.position.y > 0 && Camera.main.ScreenToWorldPoint(touch1.position).y > 0)
+            {
+                mousePosition = Camera.main.ScreenToWorldPoint(touch1.position);
+            } else if (this.transform.position.y > 0 && Camera.main.ScreenToWorldPoint(touch2.position).y > 0)
+            {
+                mousePosition = Camera.main.ScreenToWorldPoint(touch2.position);
+            }
+            else if (this.transform.position.y < 0 && Camera.main.ScreenToWorldPoint(touch2.position).y < 0)
+            {
+                mousePosition = Camera.main.ScreenToWorldPoint(touch2.position);
+            } else if (this.transform.position.y < 0 && Camera.main.ScreenToWorldPoint(touch1.position).y < 0)
+            {
+                mousePosition = Camera.main.ScreenToWorldPoint(touch1.position);
+            }
+            else {
+                mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else if (tapCount == 1)
+        {
+                touch1 = Input.GetTouch(0);
+                mousePosition = Camera.main.ScreenToWorldPoint(touch1.position);
+                //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        } else
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+
+        //Checks whether the mouse is over the sprite
+        //bool overSprite = this.GetComponent<SpriteRenderer>().bounds.Contains(mousePosition);
+
+        //If it's over the sprite
+        //if (overSprite)
+        {
+            //If we've pressed down on the mouse (or touched on the iphone)
+            if (this.gameObject.name == "player1" || this.gameObject.name == "player2" )
+            {
+                //Set the position to the mouse position
+                if (mousePosition.y > 0 && this.gameObject.name == "player2")
+                {
+                    this.transform.position = new Vector3(mousePosition.x,
+                                                        this.transform.position.y,
+                                                          0.0f);
+                } else if (mousePosition.y < 0 && this.gameObject.name == "player1") {
+                    this.transform.position = new Vector3(mousePosition.x,
+                                                        this.transform.position.y,
+                                                          0.0f);
+
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ TAP TAP SHOOT BLOCK/Assets/Code/MoveBall.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveBall : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //SoundManager.instance.PlaySingle();
+
+        
+
+        if (col.gameObject.name == "player1" || col.gameObject.name == "player2")
+        {
+            // play hit music
+            //FindObjectOfType<AudioManager>().Play("hitRacket");
+            Vector2 newDirection;
+            if (col.transform.position.y > 0)
+            {
+                newDirection = new Vector2(HitFactor(transform.position, col.transform.position, col.collider.bounds.size.x), -1).normalized;
+            } else
+            {
+                newDirection = new Vector2(HitFactor(transform.position, col.transform.position, col.collider.bounds.size.x), 1).normalized;
+            }
+            GetComponent<Rigidbody2D>().velocity = newDirection * speed;
+            this.speed *= 1.05f;
+            if (this.speed > 15)
+            {
+                this.speed = 15;
+            }
+        }
+
+        if (col.gameObject.name == "border_bottom" || col.gameObject.name == "border_up")
+        {
+            //FindObjectOfType<GameManager>().GameStop();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (col.gameObject.name == "brick")
+        {
+            //FindObjectOfType<GameManager>().GameStop();
+            Destroy(col.gameObject);
+        }
+    }
+
+
+    float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x); // racketWidth);
+    }
+}
